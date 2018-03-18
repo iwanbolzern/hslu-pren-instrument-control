ca27797def4194317b31add7a8a4bafa3ec37afd
Iwan Bolzern <iwan.bolzern@bluewin.ch>
2018-03-18 14:45:23 +0100

    application beautified

--- Seilbahn/Sources/application.c
+++ Seilbahn/Sources/application.c
@@ -6,52 +6,118 @@
  */
 
 #include "application.h"
-#include "FRTOS1.h"
 
-typedef enum applicationState applicationState_t;
-enum applicationState {
-	IDLE = 1, READING = 2, WRITING = 3
-};
-applicationState_t applicationState = IDLE;
+typedef enum imCmd {
+	INIT_TELE = 1,
+	DRIVE_DISTANCE = 2,
+	DRIVE_JOG = 3,
+	DRIVE_TO_END = 4,
+	MOVE_TELE = 5,
+	ENABLE_MAGNET = 6,
+	DISABLE_MAGNET = 7
+} imCmd_t;
 
-char commandByte[7];
+char* getCmdStream(char size);
+
+// handle functions
+void handleInitTele(char*);
+void handleDriveDistance(char*);
+void handleDriveJog(char*);
+void handleDriveToEnd(char*);
+void handleMoveTele(char*);
+void handleEnableMagnet(char*);
+void handleDisableMagnet(char*);
 
 void application(void* pvParameter) {
-	int i = 0;
-
 	for (;;) {
 		if (!queue_isEmpty(commandQueue)) {	//Sind Inhalte in der commandQueue werden diese gelesen
-			commandByte[0] = queue_read(commandQueue);
-			commandByte[1] = queue_read(commandQueue);//Anhand dem zweiten Byte in der commandQueue kann die Anzahl folgender Bytes bestimmt werden
-			for (i; i < commandByte[1]; i++) {//Alle restlichen zum Kommande gehörenden Bytes werden in das commandByte Array abgelegt
-				commandByte[i + 2] = queue_read(commandQueue);
-			}
-			if ((commandByte[2] == 2) || (commandByte[2] == 4)) {//Wenn driveDistance oder driveToEnd werden 7Bytes in die driveQueue geschrieben
-				for (int j = 0; j < 7; j++) {
-					queue_write(driveQueue, commandByte[j]);
-				}
-			}
-			else if (commandByte[2] == 3) {//Wenn driveJog werden 5Bytes in die driveQueue geschrieben
-				for (int j = 0; j < 5; j++) {
-					queue_write(driveQueue, commandByte[j]);
-				}
-			}
-			else if ((commandByte[2] == 1) || (commandByte[2] == 7)) {//Wenn InitTele oder DisableMagnet werden 3Bytes in die driveTelescopeQueue geschrieben
-				queue_write(driveTelescopeQueue, commandByte[2]);
+			char cmdSize = queue_read(commandQueue);
+			char* cmdStream = getCmdStream(cmdSize);
 
+			switch(cmdStream[0]) { // you ned ++ to skip command byte
+				case INIT_TELE:
+					handleInitTele(++cmdStream);
+					break;
+				case DRIVE_DISTANCE:
+					handleDriveDistance(++cmdStream);
+					break;
+				case DRIVE_JOG:
+					handleDriveJog(++cmdStream);
+					break;
+				case DRIVE_TO_END:
+					handleDriveToEnd(++cmdStream);
+					break;
+				case MOVE_TELE:
+					handleMoveTele(++cmdStream);
+					break;
+				case ENABLE_MAGNET:
+					handleEnableMagnet(++cmdStream);
+					break;
+				case DISABLE_MAGNET:
+					handleDisableMagnet(++cmdStream);
+					break;
 			}
-			else if (commandByte[2] == 5) {//Wenn MoveTele werden 5Bytes in die driveTelescopeQueue geschrieben
-				for (int j = 2; j < 7; j++) {
-					queue_write(driveTelescopeQueue, commandByte[j]);
-				}
-			}
-			else {		//Bei EnableMagnet -> 4Bytes zu driveTelescopeQueue
-				for (int j = 2; j < 4; j++) {
-					queue_write(driveTelescopeQueue, commandByte[j]);
-				}
-			}
+
+			free(cmdStream);
+		} else {
 			vTaskDelay(pdMS_TO_TICKS(10));
 		}
+	}
+}
+
+char* getCmdStream(char size) {
+	// wait until queue has all bytes
+	while(queue_size(commandQueue) <= size) {
 		vTaskDelay(pdMS_TO_TICKS(10));
 	}
+
+	char* cmdStream = malloc(sizeof(char) * size);
+	for(int i = 0; i < size; i++)
+		cmdStream[i] = queue_read(commandQueue);
+
+	return cmdStream;
+}
+
+void handleInitTele(char* cmdStream) {
+	// Wenn InitTele wird 1 Byte in die driveTelescopeQueue geschrieben
+	queue_write(driveTelescopeQueue, cmdStream[2]);
+}
+
+void handleDriveDistance(char* cmdStream) {
+	//Wenn driveDistance oder driveToEnd werden 4 Bytes in die driveQueue geschrieben
+	for (int i = 0; i < 4; i++) {
+		queue_write(driveQueue, cmdStream[i]);
+	}
+}
+
+void handleDriveJog(char* cmdStream) {
+	//Wenn driveJog werden 2 Bytes in die driveQueue geschrieben
+	for (int i = 0; i < 2; i++) {
+		queue_write(driveQueue, cmdStream[i]);
+	}
+}
+
+void handleDriveToEnd(char* cmdStream) {
+	//Wenn driveDistance oder driveToEnd werden 4 Bytes in die driveQueue geschrieben
+	for (int i = 0; i < 4; i++) {
+		queue_write(driveQueue, cmdStream[i]);
+	}
+}
+
+void handleMoveTele(char* cmdStream) {
+	//Wenn MoveTele werden 3 Bytes in die driveTelescopeQueue geschrieben
+	for (int i = 0; i < 3; i++) {
+		queue_write(driveTelescopeQueue, cmdStream[i]);
+	}
+}
+
+void handleEnableMagnet(char* cmdStream) {
+	if(cmdStream[1] == 0)
+		queue_write(magnetCmdQueue, RELEASE);
+	else
+		queue_write(magnetCmdQueue, RELEASE);
+}
+
+void handleDisableMagnet(char* cmdSteam) {
+	queue_write(magnetCmdQueue, DISABLE);
 }
--- Seilbahn/Sources/application.h
+++ Seilbahn/Sources/application.h
@@ -8,18 +8,18 @@
 #ifndef SOURCES_APPLICATION_H_
 #define SOURCES_APPLICATION_H_
 
+#include <stdlib.h>
+
+#include "FRTOS1.h"
 #include "PE_Types.h"
 
 #include "custom_queue.h"
-#include "queue.h"
-
+#include "magnet.h"
 
 void application(void*);
-
 
 extern QueueHandle_t driveQueue;
 extern QueueHandle_t driveTelescopeQueue;
 extern QueueHandle_t commandQueue;
-
 
 #endif /* SOURCES_APPLICATION_H_ */
