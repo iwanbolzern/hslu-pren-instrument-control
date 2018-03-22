177f61bafa119930e71e4f993e0ef8502e412307
Iwan Bolzern <iwan.bolzern@ihomelab.ch>
2018-03-22 11:47:12 +0100

    started refactoring of move tele

--- Seilbahn/Sources/driveTelescope.c
+++ Seilbahn/Sources/driveTelescope.c
@@ -22,12 +22,12 @@
 #define ONE_REVOLUTION 200
 #define TICKS_PER_MM ONE_REVOLUTION/Umfang // [mm]
 
-typedef enum {
-	IDLE, SET_UP, INIT_TELE, MOVE_TELE, MEASURE, STOP
-} state_t;
+typedef enum teleDirection {
+	teleDirection_RETRACT = 0,
+	teleDirection_EXTEND = 1
+} teleDirection_t;
 
 enum speedmode {
-
 	MODE_HYPERSLOW = 1,
 	MODE_ULTRASLOW,
 	MODE_VERYSLOW,
@@ -39,19 +39,12 @@ enum speedmode {
 	MODE_HYPERFAST
 };
 
-state_t state;
 int nTicks;
 int counterTelescope;
 LDD_TDeviceData *MyPPG1Ptr;
 char directionTelescope;
 extern bool zEndSwitch_pressed;
 
-int runden(float x) {
-	if (x > 0)
-		return (int) (x + 0.5);
-
-	return (int) (x - 0.5);
-}
 
 void setDirectionTelescope(char direction) {
 	Bit1_PutVal(direction);	//PTC13
@@ -62,14 +55,42 @@ int getTicksToGo(int distance) {
 	return distance * TICKS_PER_MM;
 }
 
+void tele_handleInitTele(void) {
+	setDirectionTelescope(teleDirection_RETRACT);
+
+	if (PPG1_Enable(MyPPG1Ptr) == ERR_OK) {			// Error handling
+//		PPG1_SelectPeriod(MyPPG1Ptr, MODE_ULTRASLOW);
+	}
+
+	while (!zEndSwitch_pressed) {					// einfahren bis Endschalter erreicht
+		vTaskDelay(pdMS_TO_TICKS(5));
+	}
+	PPG1_Disable(MyPPG1Ptr);
+}
+
+void tele_handleDriveTele(int distance, char direction) {
+	nTicks = getTicksToGo(distance);
+	counterTelescope = 0;			// counter zurückstellen
+	setDirectionTelescope(directionTelescope);
+
+
+	if (PPG1_Enable(MyPPG1Ptr)== ERR_OK) {			// Error handling
+//		PPG1_SelectPeriod(MyPPG1Ptr, MODE_MEDIUM);
+
+	}
+	else {
+		// error handling
+	}
+	while (nTicks > 0) {
+
+					vTaskDelay(pdMS_TO_TICKS(5));			//200Hz
+				}
+	// STOP Pulsgenerator
+				PPG1_Disable(MyPPG1Ptr);
+}
+
 LDD_TDeviceData* zEndSwitchPtr;
 void driveTelescope(void * pvParameter) {
-	int param1;
-	int param2;
-	int temp;
-	int counter_old ;
-	int tickCounter;
-	int distance;
 	zEndSwitchPtr = endSwitch_tele_Init(NULL);
 	MyPPG1Ptr = NULL;
 	MyPPG1Ptr = PPG1_Init(NULL);//  unter properties "enable in init. code" ankreuzen falls etwas nicht funktioniert
@@ -77,90 +98,23 @@ void driveTelescope(void * pvParameter) {
 	// Fehlerhandling falls initialisierung fehlschlägt
 
 	for (;;) {
+		while (queue_isEmpty(driveTelescopeQueue)) {
+			vTaskDelay(pdMS_TO_TICKS(20));			// 50Hz
+		}
+		char cmd = queue_read(driveTelescopeQueue);
 
-		switch (state) {
-
-		case IDLE:
-			while (queue_isEmpty(driveTelescopeQueue)) {
-				vTaskDelay(pdMS_TO_TICKS(20));			// 50Hz
-			}
-			state = SET_UP;
-			break;
-
-		case SET_UP:
-
-			switch (queue_read(driveTelescopeQueue)) {
+		switch(cmd)  {
 			case telescopeCmd_INIT_TELE:
-				state = INIT_TELE;
+				tele_handleInitTele();
 				break;
-			case telescopeCmd_DRIVE_TELE:
-
-
-
-				distance = (queue_read(driveTelescopeQueue))*256 + queue_read(driveTelescopeQueue);
-
-
-				nTicks = getTicksToGo(distance);
-
+			case telescopeCmd_DRIVE_TELE: {
+				int distance = queue_read(driveTelescopeQueue);
+				distance <<= 8;
+				distance += queue_read(driveTelescopeQueue);
 				directionTelescope = queue_read(driveTelescopeQueue);
-				state = MOVE_TELE;
+				tele_handleDriveTele(distance, directionTelescope);
 				break;
 			}
-
-			break;
-
-		case INIT_TELE:
-
-			setDirectionTelescope(0x00);		//einfahren
-
-			if (PPG1_Enable(MyPPG1Ptr) == ERR_OK) {			// Error handling
-		//		PPG1_SelectPeriod(MyPPG1Ptr, MODE_ULTRASLOW);
-			}
-
-			// drive Slowly (Einfahren)
-
-			while (!zEndSwitch_pressed) {					// einfahren bis Endschalter erreicht
-				vTaskDelay(pdMS_TO_TICKS(5));
-			}
-
-			PPG1_Disable(MyPPG1Ptr);
-			state = IDLE;
-			break;
-
-		case MOVE_TELE:
-
-			counterTelescope = 0;			// counter zurückstellen
-			setDirectionTelescope(directionTelescope);
-
-
-			if (PPG1_Enable(MyPPG1Ptr)== ERR_OK) {			// Error handling
-		//		PPG1_SelectPeriod(MyPPG1Ptr, MODE_MEDIUM);
-
-			}
-			else {
-				// error handling
-			}
-			// je nach nTicks eine entsprechende Geschwindigkeit festlegen oder einfach immer die selbe geschwindigkeit
-
-			state = MEASURE;
-			break;
-
-		case MEASURE:
-			while (nTicks > 0) {
-
-				vTaskDelay(pdMS_TO_TICKS(5));			//200Hz
-			}
-
-			state = STOP;
-
-			break;
-
-		case STOP:
-
-			// STOP Pulsgenerator
-			PPG1_Disable(MyPPG1Ptr);
-			state = IDLE;
-			break;
 		}
 	}
 }
